e292e8739abdd00ee256a624b4f9cf0d5d806cdd
Maxim <chemaxa@gmail.com>
2015-12-28 16:04:23 +0300

    work...

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,30 @@
 'use strict';
 let Todo = require('./config/models'),
+    logger = require('koa-logger'),
     router = require('koa-router')();
 
 module.exports = function(app) {
-    //
+    //Return all todos
     router.get('/api/todos', function*() {
-        //Return all todos
         this.body = yield Todo.find();
+
+    });
+
+    // create todo and send back all todos after creation
+    router.post('/api/todos', function*() {
+        this.type = 'json';
+        this.body = JSON.stringify({
+            "id": "123",
+            "string": "Stroka"
+        });
+        this.body = "Hello";
+        // create a todo, information comes from AJAX request from Angular
+        /*this.body = yield Todo.insert({
+            text: req.body.text,
+            done: false
+        });*/
+
+
     });
 
     app.use(router.routes());
